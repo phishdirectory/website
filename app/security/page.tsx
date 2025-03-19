908e480d75547bd1cdf9ed8734cdb590260758b3
Jasper Mayone <me@jaspermayone.com>
2025-03-18 22:36:44 -0400

    security page

---
+++ app/security/page.tsx
@@ -0,0 +1,20 @@
+export default function Security() {
+  return (
+    <>
+      <iframe
+        src="https://hackerone.com/a034302b-00b2-47b5-a7fe-3ced0e1480b1/embedded_submissions/new"
+        title="Submit Vulnerability Report"
+        style={{ border: 'none', width: '100%', height: '1000px' }}
+      />
+    </>
+    // <div>
+    //   <script
+    //     async
+    //     src="https://hackerone.com/a034302b-00b2-47b5-a7fe-3ced0e1480b1/embedded_submissions/script"
+    //     data-url="https://hackerone.com/a034302b-00b2-47b5-a7fe-3ced0e1480b1/embedded_submissions/new"
+    //     data-name="h1-embedded-submission"
+    //     type="text/javascript"
+    //   ></script>
+    // </div>
+  )
+}
