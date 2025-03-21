2b260688fdf413e729f0a4b33e3214b8f74623aa
Jasper Mayone <jasper.mayone@phish.directory>
2025-03-21 16:24:33 -0400

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.sanity.io'],
+        remotePatterns: ['cdn.sanity.io'],
     },
 }
+
 
 export default nextConfig;
