ddc164c68d7ba5bc10ae39791e983fbb7e698bdc
Jasper Mayone <me@jaspermayone.com>
2025-03-28 22:30:34 -0400

    move metadata to object and fix script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,140 @@
 import clsx from 'clsx'
 import { type Metadata } from 'next'
 import { Inter, Lexend } from 'next/font/google'
-import Head from 'next/head'
 
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+  },
   title: {
     template: '%s - Phish Directory',
     default: 'Phish Directory',
   },
+  authors: [
+    {
+      name: 'Jasper Mayone',
+      url: 'https://jaspermayone.com',
+    },
+  ],
+  creator: 'Jasper Mayone',
+  publisher: 'phish.directory',
+  applicationName: 'Phish Directory',
   description:
     'Stay safe from phishing attacks. Phish Directory is a community-driven database of phishing URLs.',
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicon-32x32.png?v=2',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        url: '/android-chrome-192x192.png?v=2',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicon-16x16.png?v=2',
+      },
+    ],
+    shortcut: '/favicon.ico?v=2',
+    apple: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '57x57',
+        url: '/apple-touch-icon-57x57.png?v=2',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '60x60',
+        url: '/apple-touch-icon-60x60.png?v=2',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '72x72',
+        url: '/apple-touch-icon-72x72.png?v=2',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        url: '/apple-touch-icon-76x76.png?v=2',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '114x114',
+        url: '/apple-touch-icon-114x114.png?v=2',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        url: '/apple-touch-icon-120x120.png?v=2',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '144x144',
+        url: '/apple-touch-icon-144x144.png?v=2',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '152x152',
+        url: '/apple-touch-icon-152x152.png?v=2',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        url: '/apple-touch-icon-180x180.png?v=2',
+      },
+    ],
+  },
+  openGraph: {
+    title: 'Phish Directory',
+    description:
+      'Stay safe from phishing attacks. Phish Directory is a community-driven database of phishing URLs.',
+    url: 'https://phish.directory',
+    siteName: 'Phish Directory',
+  },
+  manifest: '/site.webmanifest?v=2',
+  keywords: [
+    'phishing',
+    'phishing URLs',
+    'phishing database',
+    'phishing directory',
+    'phishing protection',
+    'phishing attacks',
+    'phishing prevention',
+    'phishing detection',
+    'phishing threats',
+    'phishing scams',
+    'phishing awareness',
+    'phishing safety',
+    'phishing education',
+    'phishing resources',
+    'phishing community',
+    'phishing reporting',
+    'phishing analysis',
+    'phishing intelligence',
+    'phishing research',
+    'phishing trends',
+    'phishing statistics',
+    'phishing news',
+    'phishing updates',
+    'phishing alerts',
+  ],
+  robots: {
+    index: true,
+    follow: true,
+  },
 }
 
 const inter = Inter({
@@ -42,92 +163,11 @@ export default function RootLayout({
         lexend.variable,
       )}
     >
-      <Head>
-        <meta
-          name="description"
-          content="Stay safe from phishing attacks. Phish Directory is a community-driven database of phishing URLs."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Phish Directory</title>
-        <link rel="icon" href="/favicon.ico" />
-        <script defer src="https://umami.hogwarts.dev/script.js" data-website-id="edb8204a-3c17-4b8e-93c2-cf991c41bd52"></script>
-        <link
-          rel="apple-touch-icon"
-          sizes="57x57"
-          href="/apple-touch-icon-57x57.png?v=2"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/apple-touch-icon-60x60.png?v=2"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/apple-touch-icon-72x72.png?v=2"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/apple-touch-icon-76x76.png?v=2"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/apple-touch-icon-114x114.png?v=2"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/apple-touch-icon-120x120.png?v=2"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/apple-touch-icon-144x144.png?v=2"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/apple-touch-icon-152x152.png?v=2"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon-180x180.png?v=2"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png?v=2"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/android-chrome-192x192.png?v=2"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png?v=2"
-        />
-        <link rel="manifest" href="/site.webmanifest?v=2" />
-        <link
-          rel="mask-icon"
-          href="/safari-pinned-tab.svg?v=2"
-          color="#5bbad5"
-        />
-        <link rel="shortcut icon" href="/favicon.ico?v=2" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta
-          name="msapplication-TileImage"
-          content="/mstile-144x144.png?v=2"
-        />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
+      <Script
+        defer={true}
+        src="https://umami.hogwarts.dev/script.js"
+        data-website-id="edb8204a-3c17-4b8e-93c2-cf991c41bd52"
+      />
       <body className="flex min-h-full flex-col">
         <Header />
         {children}
