6925fe178f56942822321cf9f8a825a1a9d23e1a
Jasper Mayone <jasper.mayone@phish.directory>
2025-06-03 09:56:37 -0400

    auth redirects

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
             hostname: 'cdn.sanity.io',
         }],
     },
+    redirects: async () => {
+        return [
+            {
+                source: '/login',
+                destination: 'https://veritas.phish.com/login',
+                permanent: true
+            },
+            {
+                source: '/signup',
+                destination: 'https://veritas.phish.com/signup',
+            }
+        ]
+    },
 }
 
 
